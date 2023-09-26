ad3b8330e141e81f1adddce709f3e8dbace192aa
juliocaa <134210748+juliocaa@users.noreply.github.com>
2023-09-26 19:21:21 +0200

    Update calcoche.py

--- calcoche.py
+++ calcoche.py
@@ -6,13 +6,16 @@ from PIL import Image
 image = Image.open("mi_logotipo.png")
 st.image(image, caption="AUTOINGENIUM", use_column_width=True)
 
-def calculate_cost(purchase_price, fuel_price, consumption, insurance, financing, months, interest_rate, annual_km, years, maintenance):
+def calculate_cost(purchase_price, fuel_price, consumption, insurance, financing, months, interest_rate, annual_km, years, maintenance, incentive=0):
     if financing:
         monthly_payment = (purchase_price * interest_rate / 12) / (1 - (1 + interest_rate / 12)**(-months))
         purchase_cost = monthly_payment * months
+        # Restamos el incentivo de cada cuota mensual
+        monthly_payment -= incentive / months
     else:
         monthly_payment = 0
-        purchase_cost = purchase_price
+        # Restamos el incentivo del coste total de compra
+        purchase_cost = purchase_price - incentive
 
     fuel_cost = (annual_km / 100) * fuel_price * consumption
     total_cost = purchase_cost + (fuel_cost + insurance + maintenance) * years
@@ -24,6 +27,7 @@ st.title("Calculadora coste de coche eléctrico vs combustión")
 
 # Datos sin valores predeterminados
 electric_purchase_price = st.number_input("Coste de adquisición del coche eléctrico", value=0)
+electric_incentive = st.number_input("Incentivos del estado para el coche eléctrico", value=0)  # Nueva casilla
 combustion_purchase_price = st.number_input("Coste de adquisición del coche de combustión", value=0)
 years = st.number_input("Años para el cálculo del coste total", value=5)
 
@@ -50,14 +54,13 @@ fuel_prices = {"Gasolina": gasoline_price, "Diésel": diesel_price, "Electricida
     
 electric_monthly, electric_total = calculate_cost(
     electric_purchase_price, fuel_prices["Electricidad"], electric_consumption, electric_insurance,
-    financing, months, electric_interest_rate, annual_km, years, electric_maintenance  # electric_interest_rate actualizado
+    financing, months, electric_interest_rate, annual_km, years, electric_maintenance, electric_incentive  # electric_incentive añadido
 )
     
 combustion_monthly, combustion_total = calculate_cost(
     combustion_purchase_price, fuel_prices[gas_type], combustion_consumption, combustion_insurance,
-    financing, months, combustion_interest_rate, annual_km, years, combustion_maintenance  # combustion_interest_rate actualizado
+    financing, months, combustion_interest_rate, annual_km, years, combustion_maintenance
 )
-
 # Crear tabla de resultados
 df = pd.DataFrame({
     "Tipo": ["Eléctrico", "Combustión"],
