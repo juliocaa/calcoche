6e753168ed2a0ac89cbd230d420f13d118246e05
juliocaa <134210748+juliocaa@users.noreply.github.com>
2023-09-26 15:25:21 +0200

    Update calcoche.py

--- calcoche.py
+++ calcoche.py
@@ -1,53 +1,63 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Comparador de coste de coches: Combustión vs Eléctrico')
+def calculate_cost(price_fuel, consumption, distance, acquisition_cost, insurance_cost, maintenance_cost, financing_months=84, interest_rate=0.06):
+    if financing_months > 0:
+        monthly_payment = acquisition_cost * (interest_rate/12) / (1 - (1 + interest_rate/12)**(-financing_months))
+    else:
+        monthly_payment = acquisition_cost / 12
+    
+    fuel_cost = (distance / 100) * consumption * price_fuel
+    total_monthly_cost = monthly_payment + fuel_cost + (insurance_cost/12) + (maintenance_cost/12)
+    
+    if financing_months > 0:
+        post_financing_cost = fuel_cost + (insurance_cost/12) + (maintenance_cost/12)
+    else:
+        post_financing_cost = total_monthly_cost
+    
+    return total_monthly_cost, post_financing_cost
 
-# Datos comunes
-coste_adquisicion_combustion = st.number_input("Coste de adquisición del coche de combustión (€)", value=0)
-coste_adquisicion_electrico = st.number_input("Coste de adquisición del coche eléctrico (€)", value=0)
-coste_anual_seguro = st.number_input("Coste anual del seguro (€)", value=0)
-compra_financiada = st.checkbox("Compra mediante financiación")
-if compra_financiada:
-    meses_financiacion = st.number_input("Meses de financiación", value=84)
-    intereses = st.number_input("Intereses (%)", value=6)
-km_anuales = st.number_input("Kilómetros esperados al año", value=20000)
-years = st.number_input("Años a considerar para el cálculo", value=5)
+# Streamlit UI
+st.title("Calculadora de coste de coche: Combustión vs Eléctrico")
+
+# Datos de entrada sin valores por defecto
+acquisition_cost_combustion = st.number_input("Coste de adquisición del coche de combustión", value=0)
+acquisition_cost_electric = st.number_input("Coste de adquisición del coche eléctrico", value=0)
+years = st.number_input("Años a tener en cuenta", value=5)
 
 # Desplegable para más opciones
-with st.expander("Más opciones"):
-    # Combustión
-    tipo_combustible = st.selectbox("Tipo de combustible", options=["Gasolina", "Diésel"])
-    if tipo_combustible == "Gasolina":
-        precio_gasolina = st.number_input("Precio gasolina (€/l)", value=1.7)
-        consumo_gasolina = st.number_input("Consumo gasolina (l/100km)", value=7)
-    else:
-        precio_gasoil = st.number_input("Precio gasoil (€/l)", value=1.5)
-        consumo_gasoil = st.number_input("Consumo gasoil (l/100km)", value=6)
-    coste_mantenimiento_combustion = st.number_input("Coste mantenimiento anual combustión (€)", value=300)
+with st.beta_expander("Más opciones"):
+    fuel_type = st.selectbox("Tipo de combustible", ["Gasolina", "Gasoil"])
+    insurance_cost_combustion = st.number_input("Coste anual del seguro para el coche de combustión", value=500)
+    insurance_cost_electric = st.number_input("Coste anual del seguro para el coche eléctrico", value=500)
+    distance = st.number_input("Kilómetros anuales", value=20000)
+    financing_months = st.number_input("Meses de financiación", value=84)
+    interest_rate = st.number_input("Tasa de interés", value=6.0) / 100
 
-    # Eléctrico
-    precio_kwh = st.number_input("Precio electricidad (€/kWh)", value=0.15)
-    consumo_electricidad = st.number_input("Consumo electricidad (kWh/100km)", value=17)
-    coste_mantenimiento_electrico = st.number_input("Coste mantenimiento anual eléctrico (€)", value=100)
+# Constantes
+default_values = {
+    "Gasolina": {"price": 1.7, "consumption": 7, "maintenance": 300},
+    "Gasoil": {"price": 1.5, "consumption": 6, "maintenance": 300},
+    "Eléctrico": {"price": 0.15, "consumption": 17, "maintenance": 100},
+}
 
 # Cálculos
-if compra_financiada:
-    cuota_financiacion_combustion = coste_adquisicion_combustion * (intereses / 100 / 12) / (1 - (1 + intereses / 100 / 12) ** -meses_financiacion)
-    cuota_financiacion_electrico = coste_adquisicion_electrico * (intereses / 100 / 12) / (1 - (1 + intereses / 100 / 12) ** -meses_financiacion)
-else:
-    cuota_financiacion_combustion = 0
-    cuota_financiacion_electrico = 0
+total_combustion, post_combustion = calculate_cost(
+    default_values[fuel_type]["price"], default_values[fuel_type]["consumption"], distance, 
+    acquisition_cost_combustion, insurance_cost_combustion, default_values[fuel_type]["maintenance"],
+    financing_months, interest_rate
+)
 
-if tipo_combustible == "Gasolina":
-    coste_mensual_combustion = (precio_gasolina * consumo_gasolina * km_anuales / 100 + coste_anual_seguro + coste_mantenimiento_combustion) / 12 + cuota_financiacion_combustion
-else:
-    coste_mensual_combustion = (precio_gasoil * consumo_gasoil * km_anuales / 100 + coste_anual_seguro + coste_mantenimiento_combustion) / 12 + cuota_financiacion_combustion
+total_electric, post_electric = calculate_cost(
+    default_values["Eléctrico"]["price"], default_values["Eléctrico"]["consumption"], distance, 
+    acquisition_cost_electric, insurance_cost_electric, default_values["Eléctrico"]["maintenance"],
+    financing_months, interest_rate
+)
 
-coste_mensual_electrico = (precio_kwh * consumo_electricidad * km_anuales / 100 + coste_anual_seguro + coste_mantenimiento_electrico) / 12 + cuota_financiacion_electrico
-
-# Mostrar resultados
-st.subheader("Resultados")
-st.write(f"Coste mensual del coche de combustión: €{coste_mensual_combustion:.2f}")
-st.write(f"Coste mensual del coche eléctrico: €{coste_mensual_electrico:.2f}")
-st.write(f"Coste acumulado del coche de combustión en {years} años: €{coste_mensual_combustion * 12 * years:.2f}")
-st.write(f"Coste acumulado del coche eléctrico en {years} años: €{coste_mensual_electrico * 12 * years:.2f}")
+# Crear la tabla
+df = pd.DataFrame({
+    "Coche": ["Combustión", "Eléctrico"],
+    "Coste Mensual": [total_combustion, total_electric],
+    "Coste Post-Financiamiento": [post_combustion, post_electric]
+})
+st.table(df)
